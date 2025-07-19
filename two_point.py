2dda744302b09fa5d0ccfa9da3492f9a7df365e9
Nic McPhee <mcphee@morris.umn.edu>
2025-07-19 16:59:20 -0500

    Refactor two-point crossover animation for clarity
    Improves the structure and readability of the Manim animation scene by abstracting complex logic into dedicated helper methods.
    
    The logic for creating crossover point indicators and animating gene copying is extracted from the main `construct` method into `visualize_crossover_point` and `copy_genes` respectively. This change removes significant code duplication and makes the primary animation sequence much easier to follow.

--- two_point.py
+++ two_point.py
@@ -1,83 +1,43 @@
-from manim import *
+from manim import * # type: ignore
 
 class TwoPointCrossoverScene(Scene):
     genome_length = 10
     gene_stroke_width = 2
     gene_fill_opacity = 0.8
-    square_buffer = 0.1
+    square_buffer = 0.15
+
+    # The two crossover points will be to the _right_ of the genes
+    # with the following indices. The code assume `0 ≤ cp1_idx < cp2_idx ≤ genome_length`
+    cp1_idx = 2
+    cp2_idx = 6
+    assert 0 <= cp1_idx < cp2_idx <= genome_length
+
+    def setup(self):
+        self.parent1_genes, self.parent2_genes, self.child_genes = self.build_genomes()
+        return super().setup()
 
     def construct(self):
-        # 1. Define parameters
-        cp1_idx = 2
-        cp2_idx = 6
+        # Visualize the crossover points
 
-        parent1_genes, parent2_genes, child_genes = self.build_genomes()
+        self.visualize_crossover_point(self.cp1_idx, "Crossover Point 1")
+        self.visualize_crossover_point(self.cp2_idx, "Crossover Point 2")
 
-        # 3. Indicate Crossover Points
-        if self.genome_length == 0:
-            return
-
-        cp_line_y_start = parent1_genes.get_top()[1] + 0.1
-        cp_line_y_end = child_genes.get_bottom()[1] - 0.1
-
-        if 0 < cp1_idx <= self.genome_length:
-            cp1_x = (parent1_genes[cp1_idx-1].get_right()[0] + parent1_genes[cp1_idx].get_left()[0]) / 2
-            cp1_line = DashedLine(Point(np.array([cp1_x, cp_line_y_start, 0])), Point(np.array([cp1_x, cp_line_y_end, 0])), color=YELLOW, dash_length=0.15, dashed_ratio=0.70)
-            cp1_text = Text("Crossover Point 1", font_size=24).move_to(Point(np.array([cp1_x, cp_line_y_start + 0.3, 0])))
-            self.add(cp1_text)
-            self.play(Create(cp1_line))
-        elif cp1_idx == 0:
-            cp1_x = parent1_genes[0].get_left()[0] - self.square_buffer / 2
-            cp1_line = DashedLine(Point(np.array([cp1_x, cp_line_y_start, 0])), Point(np.array([cp1_x, cp_line_y_end, 0])), color=YELLOW, dash_length=0.15, dashed_ratio=0.70)
-            cp1_text = Text("Crossover Point 1", font_size=24).move_to(Point(np.array([cp1_x, cp_line_y_start + 0.3, 0])))
-            self.add(cp1_text)
-            self.play(Create(cp1_line))
-
-        if cp1_idx < cp2_idx < self.genome_length:
-            cp2_x = (parent1_genes[cp2_idx-1].get_right()[0] + parent1_genes[cp2_idx].get_left()[0]) / 2
-            cp2_line = DashedLine(Point(np.array([cp2_x, cp_line_y_start, 0])), Point(np.array([cp2_x, cp_line_y_end, 0])), color=YELLOW, dash_length=0.15, dashed_ratio=0.70)
-            cp2_text = Text("Crossover Point 2", font_size=24).move_to(Point(np.array([cp2_x, cp_line_y_start + 0.3, 0])))
-            self.add(cp2_text)
-            self.play(Create(cp2_line))
-        elif cp2_idx == self.genome_length and cp1_idx < cp2_idx:
-            cp2_x = parent1_genes[self.genome_length-1].get_right()[0] + self.square_buffer / 2
-            cp2_line = DashedLine(Point(np.array([cp2_x, cp_line_y_start, 0])), Point(np.array([cp2_x, cp_line_y_end, 0])), color=YELLOW, dash_length=0.15, dashed_ratio=0.70)
-            cp2_text = Text("Crossover Point 2", font_size=24).move_to(Point(np.array([cp2_x, cp_line_y_start + 0.3, 0])))
-            self.add(cp2_text)
-            self.play(Create(cp2_line))
-
-        self.wait(0.5) # Shorter wait
+        self.wait(0.5)
 
         # 4. Animate Gene Copying using TransformFromCopy
         # Segment 1: Parent 1 (indices 0 to cp1_idx - 1)
-        if cp1_idx > 0:
-            animations_seg1 = []
-            for i in range(cp1_idx):
-                # Set the child genome square to have the same color, etc., as the parent genome.
-                # We have to set `match_center` to `True` or it will also set the child's position
-                # to match parent, and nothing in the animation.
-                child_genes[i].become(parent1_genes[i], match_center=True)
-                # child_genes[i] (target) transforms from a copy of parent1_genes[i] (source)
-                animations_seg1.append(TransformFromCopy(parent1_genes[i], child_genes[i]))
-            self.play(AnimationGroup(*animations_seg1, lag_ratio=0.1))
+        if self.cp1_idx > 0:
+            self.copy_genes(self.parent1_genes, self.child_genes, range(self.cp1_idx))
             self.wait(0.5)
 
         # Segment 2: Parent 2 (indices cp1_idx to cp2_idx - 1)
-        if cp2_idx > cp1_idx:
-            animations_seg2 = []
-            for i in range(cp1_idx, cp2_idx):
-                child_genes[i].become(parent2_genes[i], match_center=True)
-                animations_seg2.append(TransformFromCopy(parent2_genes[i], child_genes[i]))
-            self.play(AnimationGroup(*animations_seg2, lag_ratio=0.1))
+        if self.cp2_idx > self.cp1_idx:
+            self.copy_genes(self.parent2_genes, self.child_genes, range(self.cp1_idx, self.cp2_idx))
             self.wait(0.5)
 
         # Segment 3: Parent 1 (indices cp2_idx to genome_length - 1)
-        if cp2_idx < self.genome_length:
-            animations_seg3 = []
-            for i in range(cp2_idx, self.genome_length):
-                child_genes[i].become(parent1_genes[i], match_center=True)
-                animations_seg3.append(TransformFromCopy(parent1_genes[i], child_genes[i]))
-            self.play(AnimationGroup(*animations_seg3, lag_ratio=0.1))
+        if self.cp2_idx < self.genome_length:
+            self.copy_genes(self.parent1_genes, self.child_genes, range(self.cp2_idx, self.genome_length))
 
         # Final wait
         self.wait(1)
@@ -96,12 +56,15 @@ class TwoPointCrossoverScene(Scene):
         child_genes: VGroup = self.build_genome(child_fill_color_initial, child_stroke_color_initial)
 
         genomes_group: VGroup = VGroup(parent1_genes, parent2_genes, child_genes).arrange(DOWN, buff=0.7)
-        self.add(genomes_group)
+        # self.add(genomes_group)
 
-        p1_label: Text = Text("Parent 1").next_to(parent1_genes, LEFT, buff=0.3)
-        p2_label: Text = Text("Parent 2").next_to(parent2_genes, LEFT, buff=0.3)
-        child_label: Text = Text("Child").next_to(child_genes, LEFT, buff=0.3)
-        self.add(p1_label, p2_label, child_label)
+        p1_label: Text = Text("Parent 1").next_to(parent1_genes, LEFT, buff=2*self.square_buffer)
+        p2_label: Text = Text("Parent 2").next_to(parent2_genes, LEFT, buff=2*self.square_buffer)
+        child_label: Text = Text("Child").next_to(child_genes, LEFT, buff=2*self.square_buffer)
+
+        self.add(VGroup(genomes_group, p1_label, p2_label, child_label).center())
+
+        # self.add(p1_label, p2_label, child_label)
         # Shorter wait
         self.wait(0.5)
         return parent1_genes,parent2_genes,child_genes
@@ -112,3 +75,38 @@ class TwoPointCrossoverScene(Scene):
                    stroke_color=stroke_color, stroke_width=self.gene_stroke_width)
             for _ in range(self.genome_length)
         ]).arrange(RIGHT, buff=self.square_buffer)
+
+    def visualize_crossover_point(self, gap_index, label):
+        # These specify the top and bottom y-coordinates of the dashed lines representing
+        # the crossover points. We want the lines to start slightly above the first parent
+        # (hence the +0.1) and end slightly below the child (hence the -0.1).
+        cp_line_y_start = self.parent1_genes.get_top()[1] + 0.1
+        cp_line_y_end = self.child_genes.get_bottom()[1] - 0.1
+
+        if gap_index < self.genome_length:
+            # Set the x-coordinate of the crossover line to be a little to the left of the gene at `gap_index`.
+            x_coordinate = self.parent1_genes[gap_index].get_left()[0] - self.square_buffer / 2
+        else:
+            # Here the crossover line is to the right of all the genes, so we set its x-coordinate to be a little
+            # to the right of the last gene.
+            x_coordinate = self.parent1_genes[-1].get_right()[0] + self.square_buffer / 2
+        line = DashedLine(
+            Point(np.array([x_coordinate, cp_line_y_start, 0])),
+            Point(np.array([x_coordinate, cp_line_y_end, 0])),
+            color=YELLOW,
+            dash_length=0.15,
+            dashed_ratio=0.70
+        )
+        text = Text(label, font_size=24).move_to(Point(np.array([x_coordinate, cp_line_y_start + 0.3, 0])))
+        self.add(text)
+        self.play(Create(line))
+
+    def copy_genes(self, from_genes, to_genes, gene_range):
+        animations = []
+        for i in gene_range:
+            # Set the target genome square to have the same color, etc., as the parent genome.
+            # We have to set `match_center` to `True` or it will also set the child's position
+            # to match parent, and nothing happens in the animation.
+            to_genes[i].become(from_genes[i], match_center=True)
+            animations.append(TransformFromCopy(from_genes[i], to_genes[i]))
+        self.play(AnimationGroup(*animations, lag_ratio=0.1))
